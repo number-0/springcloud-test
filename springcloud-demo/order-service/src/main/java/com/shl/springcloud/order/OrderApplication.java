dcdfb36e02119ef31636a741406780e4dc94cb16
songhengliang <songhengliang@51xf.cn>
2021-04-24 14:58:29 +0800

    init

--- springcloud-demo/order-service/src/main/java/com/shl/springcloud/order/OrderApplication.java
+++ springcloud-demo/order-service/src/main/java/com/shl/springcloud/order/OrderApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EntityScan("cn.itcast.order.entity")
+@EntityScan("com.shl.springcloud.order.entity")
+//激活Feign
+@EnableFeignClients
 public class OrderApplication {
 
 	/**
